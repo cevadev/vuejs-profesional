02eb4870e5cd3e24ebf7cf8911b8bf88f5eff0ba
Carlos Eduardo Villanueva Altuna <carlos.villanueva.altuna@gmail.com>
2021-06-05 23:51:55 -0300

    Musik project

--- src/App.vue
+++ src/App.vue
@@ -16,9 +16,9 @@
     p {{ `La suma de 1 + 1 es ${1+1}` }}
 
     //accediendo a propiedades de objetos
-    p {{ `Su nombre es: ${person.name}` }}
-    p {{ `Su apellido es: ${person.lastname.toUpperCase()}` }}
-    p {{ JSON.stringify(person) }}
+    p {{ `Su nombre es: ${name}` }}
+    p {{ `Su apellido es: ${lastname.toUpperCase()}` }}
+    p {{ JSON.stringify(colores) }}
     p {{ true ? 'true' : 'false'}}
 
     //Directivas en vue
@@ -29,10 +29,31 @@
 
     //directiva v-for
     ul
-      li(v-for="numero in numeros", :key="index") {{ numero }}
+      li(v-for="(numero, index) in numeros", :key="index") {{ numero }}
 
     //Data bindings
-    input(v-model="person.name")
+    input(v-model="text")
+    p {{ text }}
+
+    //v-bind (creamos un anchor con la directiva v-bind que enlaza con el atributo href que toma el valor de la propiedad url)
+    a(v-bind:href="url") Link
+    br
+    br
+
+    //Computed properties
+    input(v-model="name")
+    input(v-model="lastname")
+    p {{ `Su edad es: ${ getAge }` }}
+
+    .field
+      label.label Fecha de nacimiento
+      .control
+        input.input(v-model="birthday" type="date")
+
+    .columns
+      p {{ `Su edad es: ${ getAge }` }}
+
+    //llamamos a la computed property
 
     //Llamando un componte y props con pug
     //HelloWorld(msg="Hello Vue world")
@@ -52,13 +73,34 @@ export default {
   },
   data () {
     return {
+      colores: {
+        nombre: 'Azul',
+        ingles: 'blue'
+      },
       showValue: false,
       value: 'trabajando con directivas',
-      person: {
-        name: 'Carlos',
-        lastname: 'Villanueva'
-      },
-      numeros: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+      text: '',
+      url: 'https://www.google.com/webhp?hl=es&sa=X&ved=0ahUKEwiA1qvU0oHxAhWXqpUCHS3ICa0QPAgI',
+      name: '',
+      lastname: '',
+      numeros: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      birthday: ''
+    }
+  },
+  computed: {
+    fullname () {
+      return `Su nombre es: ${this.name} ${this.lastname}`
+    },
+    getAge () {
+      // return new Date(new Date() - new Date(this.birthday)).getFullYear - 1970
+      const today = new Date()
+      const birth = new Date(this.birthday)
+      let age = today.getFullYear() - birth.getFullYear()
+      const m = today.getMonth() - birth.getMonth()
+      if (m < 0 || (m === 0 && today.getDate() < birth.getDate())) {
+        age--
+      }
+      return age
     }
   }
 }
