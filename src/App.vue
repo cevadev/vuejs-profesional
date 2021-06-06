7217d7c40dc09438c4ca684179a7480dc1474bdc
Carlos Eduardo Villanueva Altuna <carlos.villanueva.altuna@gmail.com>
2021-06-05 21:11:37 -0300

    Musik project

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,31 @@
 
 <template lang="pug">
   #app
-    MovieView
+    //Expresiones en vue
+    h1 {{ 'Hello Vue'}}
+    p {{ `La suma de 1 + 1 es ${1+1}` }}
+
+    //accediendo a propiedades de objetos
+    p {{ `Su nombre es: ${person.name}` }}
+    p {{ `Su apellido es: ${person.lastname.toUpperCase()}` }}
+    p {{ JSON.stringify(person) }}
+    p {{ true ? 'true' : 'false'}}
+
+    //Directivas en vue
+    p(v-show="showValue") {{ value }}
+    p(v-if="showValue") {{ value }}
+    p(v-else-if="true") {{ 'mostrando directivas en Vue' }}
+    p(v-else) {{ 'Directivas' }}
+
+    //directiva v-for
+    ul
+      li(v-for="numero in numeros", :key="index") {{ numero }}
+
+    //Data bindings
+    input(v-model="person.name")
+
+    //Llamando un componte y props con pug
+    //HelloWorld(msg="Hello Vue world")
 </template>
 
 <script>
@@ -25,6 +49,17 @@ export default {
     HelloWorld,
     RegisterSample,
     MovieView
+  },
+  data () {
+    return {
+      showValue: false,
+      value: 'trabajando con directivas',
+      person: {
+        name: 'Carlos',
+        lastname: 'Villanueva'
+      },
+      numeros: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    }
   }
 }
 </script>
