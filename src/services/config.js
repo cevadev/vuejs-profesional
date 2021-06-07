bbbe068f0a0c588b9a3e55dec255776d1167b9b0
Carlos Eduardo Villanueva Altuna <carlos.villanueva.altuna@gmail.com>
2021-06-07 11:02:23 -0300

    Musik project

---
+++ src/services/config.js
@@ -0,0 +1,8 @@
+//storage de archivos de configuracion
+//objeto configService
+const configService = {
+  apiUrl: "https://platzi-music-api.herokuapp.com/"
+};
+
+//exportamos el objeto para ser utilizado
+export default configService;
---
+++ src/services/platzi-music.js
@@ -0,0 +1,13 @@
+/**
+ * platziMusicService - Api que nos permite interactuar con la api de Platzi
+ */
+import trae from "trae";
+import configService from "./config";
+
+//creamos una nueva instancia de trae y la guardamos en el objeto platziMusicService
+const platziMusicService = trae.create({
+  baseUrl: configService.apiUrl
+});
+
+//exportamos nuestro objeto platziMusicService
+export default platziMusicService;
